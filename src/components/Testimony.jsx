b2195e605bc89c821322ac22984f964282520220
SayefIftekhar <117587659+SayefIftekhar@users.noreply.github.com>
2024-11-21 01:21:58 +0600

    Update Testimony.jsx
    Testimony Updated
    Image Size updated

--- src/components/Testimony.jsx
+++ src/components/Testimony.jsx
@@ -1,13 +1,10 @@
 "use client";
 import React, { useEffect, useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
-
 import "./testimony.css";
-
 // import required modules
 import { Navigation } from "swiper/modules";
 import Image from "next/image";
@@ -55,7 +52,7 @@ const Testimony = () => {
 };
 const testimony = [
   {
-    image: "https://i.ibb.co.com/31YvMzg/2ad3ce0f-6633-426c-8f04-e6380b765021-20241120-230914-0000.png",
+    image: "https://i.ibb.co.com/sJMyk6C/Mahfuzur-Rahman.png",
     name: "Mahfuzur Rahman",
     title: "CEO, T-shirt Karkhana",
     comment:
@@ -63,7 +60,7 @@ const testimony = [
     video: "",
   },
   {
-    image: "https://i.ibb.co.com/Dw4TVJq/Shahriar-Shanto.png",
+    image: "https://i.ibb.co.com/Lk7sWpK/Shahriar-Shanto.png",
     name: "Shahriar Shanto",
     title: "CEO, SpangledBD",
     comment:
@@ -71,7 +68,7 @@ const testimony = [
     video: "",
   },
   {
-    image: "https://i.ibb.co.com/kKQ76fw/c7f75f27-5c74-477a-b333-0994b4a38578-20241120-231141-0000.png",
+    image: "https://i.ibb.co.com/fGmCM24/Francesco-Haze.png",
     name: "Francesco Haze",
     title: "Senior Marketing Analyst, GROW by SAVVY",
     comment:
@@ -79,7 +76,7 @@ const testimony = [
     video: "",
   },
   {
-    image: "https://i.ibb.co.com/PTVrhnR/c796ca8d-6c86-4fc9-bb52-1b8b518b74ed-20241120-231434-0000.png",
+    image: "https://i.ibb.co.com/dWjzSpv/Rowshon-Rahman.png",
     name: "Rowshonur Rahman",
     title: "COO, Ayojon Event Management",
     comment:
@@ -87,31 +84,31 @@ const testimony = [
     video: "",
   },
   {
-    image: "https://i.ibb.co.com/YQ6Dfrk/829b2a05-808e-40c7-a524-a66cbdee8345-20241120-231018-0000.png",
+    image: "https://i.ibb.co.com/0ZfFpyW/Edwardo.png",
     name: "Edowardo",
-    title: "Senior Marketing Analyst, GROW by SAVVY",
+    title: "CTO, Drago IT",
     comment:
       "Working with Mammoth Media on our music album design and promotion was a fantastic experience.",
     video: "",
   },
   {
-    image: "/artist-white.png",
+    image: "https://i.ibb.co.com/H4xcf5x/Stephen-Collins.png",
     name: "Stephen Collins",
-    title: "Senior Marketing, NXTWAY & ANIMAGIC",
+    title: "Senior Marketing, NXTWAY",
     comment:
       "Really looking forward to our next project. Our last one was a blast.",
     video: "",
   },
   {
-    image: "https://i.ibb.co.com/h90Fwsz/b0c8000c-3bda-46a4-8c72-a569e7e6b1e0-20241120-231558-0000.png",
+    image: "https://i.ibb.co.com/7yBXT7h/Tanbir-Taz.png",
     name: "Tanbir Taz",
-    title: "CEO, NXTWAY & ANIMAGIC",
+    title: "COO, ANIMAGIC",
     comment:
       "Mammoth Media captured the essence of our brand and created a visually stunning logo and brand guidelines.",
     video: "",
   },
   {
-    image: "https://i.ibb.co.com/RcVq9H0/f2761655-822e-4465-90e5-e42ba8164daf-20241120-231331-0000.png",
+    image: "https://i.ibb.co.com/WG0nMGx/Irfan-Sadique.png",
     name: "Irfan Sadique Sium",
     title: "Founder, PLEXUS FOR YOUTH",
     comment:
@@ -119,7 +116,7 @@ const testimony = [
     video: "",
   },
   {
-    image: "https://i.ibb.co.com/HhDN7DY/bab98985-519c-4ac7-96d1-2885075d3555-20241120-231233-0000.png",
+    image: "https://i.ibb.co.com/SvdfRzw/Anika-Tabassum.png",
     name: "Anika Tabassum",
     title: "Digital Creator",
     comment:
